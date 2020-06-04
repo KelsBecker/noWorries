3ba6c6cbd3422e451ef02eed8b3cc266bf769d0a
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-06-04 13:12:35 -0400

    initial commit

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hi Kelly!</Text>
+      <Text>LETS MAKE AN AWESOME APP</Text>
     </View>
   );
 }
