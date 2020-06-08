9ce27152d8e12343d72d572c25f515f98ac387d8
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-06-08 13:31:57 -0400

    login verrified

---
+++ src/CarouselDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+import Carousel from 'react-native-snap-carousel';
+
+export default function CarouselDisplay() {
+
+    return(
+        <View>
+            <Text>Carousel Goes Here</Text>
+        </View>
+    )
+}
--- src/CategoryPicker.js
+++ src/CategoryPicker.js
@@ -1,19 +1,37 @@
 import React from 'react'
+import {StyleSheet} from 'react-native'
 import RNPickerSelect from 'react-native-picker-select';
+
 
 export default function CategoryPicker(props) {
 
     return(
         <>
         <RNPickerSelect
+            style={styles.inputIOS}
             onValueChange={(value) => props.categorySelect(value)}
             items={[
                 { label: 'All Locations', value: 'All Locations' },
                 { label: 'Groceries', value: 'Groceries' },
                 { label: 'Parks', value: 'Parks' },
+                { label: 'Your Favorites', value: 'Your Favorites'}
             ]}
         />
         </>
     )
 
 }
+
+//styles not showing up
+const styles = StyleSheet.create({
+    inputIOS: {
+        fontSize: 16,
+        paddingVertical: 12,
+        paddingHorizontal: 10,
+        borderWidth: 1,
+        borderColor: 'gray',
+        borderRadius: 4,
+        color: 'black',
+        paddingRight: 30, // to ensure the text is never behind the icon
+    }
+})
